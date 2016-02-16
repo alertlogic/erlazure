84a97bde6e420665fb09482db84dcaacf4b640b6
Joseph Frangoudes <jfrangoudes@alertlogic.com>
2016-02-16 09:40:38 +0000

    Recursive resolving of tokens and now accepts a $select query

--- src/erlazure_insights.erl
+++ src/erlazure_insights.erl
@@ -27,18 +27,18 @@ get_events_all(SubscriptionId, Filter, Select, Config=#azure_config{}) ->
         _ -> follow_token(binary_to_list(NextToken), Values, Config)
     end.
 
--spec get_events(string(), string(), string(), #azure_config{}, string()) ->
+-spec get_events(string(), string(), string(), #azure_config{}, binary()) ->
     {ok, list(), undefined | binary()}.
 get_events(SubscriptionId, Filter, Select, Config=#azure_config{}, NextToken) ->
     case NextToken of
-        [] ->
+        undefined ->
             case http_request(SubscriptionId, Filter, Select, Config) of
                 {ok, _Values, _NextToken} = Response ->
                     Response;
                 {error, _Reason} = ErrorCase ->
                     ErrorCase
             end;
-        _ -> follow_token(NextToken, [], Config)
+        _ -> request_api(binary_to_list(NextToken), Config)
     end.
 
 -spec follow_token(string(), list(), #azure_config{}) ->
@@ -72,17 +72,24 @@ request_api(Url, Config) ->
 http_request(SubscriptionId, Filter, Select, Config) ->
     URL = "https://management.azure.com/subscriptions/" ++
     SubscriptionId ++
-    "/providers/microsoft.insights/eventtypes/management/values?" ++ "api-version=2014-04-01" ++ 
-    "&%24filter=" ++ Filter ++
-    "&%24select=" ++ Select,
+    "/providers/microsoft.insights/eventtypes/management/values?" ++
+    make_query_string([
+            {"api-version", "2014-04-01"},
+            {"$filter", Filter},
+            {"$select", Select}
+        ]),
     request_api(URL, Config).
+
+make_query_string(Params) ->
+    Encoded = [ K ++ "=" ++ http_uri:encode(V) || {K, V} <- Params, V =/= ""],
+    string:join(Encoded, "&").
 
 -spec create_time_bounded_filter(string(), string()) -> string().
 create_time_bounded_filter(TS1, TS2) ->
-    Filter = http_uri:encode("eventTimestamp ge '" ++
+    Filter = "eventTimestamp ge '" ++
         TS1  ++
     "' and eventTimestamp le '" ++
-    TS2 ++ "'"),
+    TS2 ++ "'",
     Filter.
 
 construct_headers(Config) ->
@@ -95,10 +102,10 @@ test_url_construction() ->
     
     ToDateTime = {{2016, 2, 3}, {14, 23, 57}},
 
-    ToTimestamp = get_time_offset_as_timestamp(ToDateTime, {seconds, 0}),
-    FromTimestamp = get_time_offset_as_timestamp(ToDateTime, {hours, 24}),
+    ToTimestamp = "To",
+    FromTimestamp = "From",
     Filter = create_time_bounded_filter(FromTimestamp, ToTimestamp),
-    Select = "I%20am%20selecting%20something",
+    Select = "eventName, eventSource",
 
     SubId = "SubId",
     Config = #azure_config{auth_token="Token"},
@@ -110,9 +117,10 @@ test_url_construction() ->
                 TestUrl = "https://management.azure.com/subscriptions/" ++ 
                 SubId ++ 
                 "/providers/microsoft.insights/eventtypes/management/values?api-version=2014-04-01&" ++
-                "%24filter=eventTimestamp%20ge%20%272016-02-02T14%3A23%3A57Z%27%20and%20eventTimestamp%20le%20%272016-02-03T14%3A23%3A57Z%27" ++ 
-                "&%24select=I%20am%20selecting%20something",
+                "$filter=eventTimestamp%20ge%20%27From%27%20and%20eventTimestamp%20le%20%27To%27" ++ 
+                "&$select=eventName%2C%20eventSource",
 
+                io:format("Expected ~p~nActual ~p~n", [TestUrl, Url]),
                 true = (TestUrl == Url),
                 {ok, {{"version", 401, "Phrase"}, "Headers", "Body"}}
         end
